302f2ab3adb4df13ed385a78dc6b7bb2f984112e
DZWG <paule22@protonmail.com>
2023-01-31 17:46:47 +0100

    updated home view

--- src/renderer/src/views/HomeView.vue
+++ src/renderer/src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home flex flex-col base-content">
-    <div class="flex flex-row justify-center space-x-2">
-      <ul class="basis-1/2 steps steps-vertical">
-        <li class="step step-primary">'Import' your own config or use the default</li>
-        <li class="step step-primary">Check you imported config in the 'Export' section</li>
-        <li class="step step-primary">Modify your hosts in the 'Host' section</li>
-        <li class="step step-primary">Modify your VMs in the 'VM' section</li>
-        <li class="step step-primary">Assign your VMs to the host in the 'Assignment' section</li>
-        <li class="step step-primary">Check the PDF in the 'PDF' section</li>
-        <li class="step step-primary">Save all your files in the 'Export' as .zip</li>
+    <div class="flex flex-row justify-center items-center space-x-2">
+      <ul class="basis-1/2 space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+        <li class="">'Import' your own config or use the default</li>
+        <li class="">Check you imported config in the 'Export' section</li>
+        <li class="">Modify your hosts in the 'Host' section</li>
+        <li class="">Modify your VMs in the 'VM' section</li>
+        <li class="">Assign your VMs to the host in the 'Assignment' section</li>
+        <li class="">Check the PDF in the 'PDF' section</li>
+        <li class="">Save all your files in the 'Export' as .zip</li>
       </ul>
       <img class="object-contain w-96" src="/logo.png" alt="app-logo" />
     </div>
   </div>
 </template>
+
+
+
 
 <script setup lang="ts"></script>
