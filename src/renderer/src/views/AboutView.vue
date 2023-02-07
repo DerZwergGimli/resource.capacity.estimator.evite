67d0de073f2a83310951e8fe0d207695f1993a3b
DZWG <paule22@protonmail.com>
2023-02-07 16:24:20 +0100

    updated icon image in about

--- src/renderer/src/views/AboutView.vue
+++ src/renderer/src/views/AboutView.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="home flex flex-col text-center base-content space-y-2">
     <div class="flex flex-row justify-center">
-      <the-card
-        href="#"
-        variant="horizontal"
-        img-src="public/logo.png"
-        img-alt="app-logo"
-        class="p-3"
-      >
+      <the-card href="#" variant="horizontal" img-src="/logo.png" img-alt="app-logo" class="p-3">
         <div class="flex flex-col space-y-5">
           <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             This tool is used for estimating hardware resources for virtual environments.
