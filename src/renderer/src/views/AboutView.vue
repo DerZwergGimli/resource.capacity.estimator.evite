70f0d5ce1fbd748939d250dddb4b86afe4da7564
DZWG <paule22@protonmail.com>
2023-02-07 15:52:01 +0100

    updated AboutView.vue

--- src/renderer/src/views/AboutView.vue
+++ src/renderer/src/views/AboutView.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="home flex flex-col text-center base-content">
-    <Button>reset app-data</Button>
-
-    <div class="flex flex-row justify-center space-x-2">
-      <p class="basis-1/2">
-        This tool is used for estimating hardware resources for virtual environments.
-      </p>
+  <div class="home flex flex-col text-center base-content space-y-2">
+    <div class="flex flex-row justify-center">
+      <the-card
+        href="#"
+        variant="horizontal"
+        img-src="public/logo.png"
+        img-alt="app-logo"
+        class="p-3"
+      >
+        <div class="flex flex-col space-y-5">
+          <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            This tool is used for estimating hardware resources for virtual environments.
+          </h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">It's used as a guidance!</p>
+          <Button>RESET App-Data</Button>
+        </div>
+      </the-card>
     </div>
+    <div class="basis-1/2"></div>
   </div>
 </template>
 <script setup lang="ts">
 import { Button } from 'flowbite-vue'
+import { TheCard } from 'flowbite-vue'
 </script>
